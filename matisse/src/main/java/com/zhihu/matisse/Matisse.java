1a9b4b1cf2a3eac8680ffd341329b2f40fb1b74e
CoXier <justcoxier@gmail.com>
2017-04-25 09:50:51 +0800

    check style

--- matisse/src/main/java/com/zhihu/matisse/Matisse.java
+++ matisse/src/main/java/com/zhihu/matisse/Matisse.java
@@ -36,7 +36,7 @@ public final class Matisse {
     private final WeakReference<Fragment> mFragment;
 
     private Matisse(Activity activity) {
-        this(activity,null);
+        this(activity, null);
     }
 
     private Matisse(Activity activity, Fragment fragment) {
