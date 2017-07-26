b25a8bc17b1fa175f500d85a5c51305ad60674fa
chenfeng <chenfeng@weidian.com>
2017-07-26 12:13:19 +0800

    add builder

--- matisse/src/main/java/com/zhihu/matisse/engine/impl/FrescoEngine.java
+++ matisse/src/main/java/com/zhihu/matisse/engine/impl/FrescoEngine.java
@@ -2,7 +2,6 @@ package com.zhihu.matisse.engine.impl;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
@@ -11,14 +10,11 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.ControllerListener;
-import com.facebook.drawee.drawable.ForwardingDrawable;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.DraweeHolder;
 import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.zhihu.matisse.engine.ImageEngine;
@@ -27,25 +23,17 @@ import com.zhihu.matisse.engine.ImageEngine;
  * {@link ImageEngine} implements with Fresco
  */
 
+@RequiresApi(api = Build.VERSION_CODES.HONEYCOMB_MR1)
 public class FrescoEngine implements ImageEngine {
 
-    private void initFresco(Context context) {
-        if (!Fresco.hasBeenInitialized()) {
-            Fresco.initialize(context);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB_MR1)
     @Override
     public void loadThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri) {
-        initFresco(context);
-
-        DraweeLoader loader = new DraweeLoader(context);
-        loader.setSize(resize);
-        loader.setImageUri(uri);
-        imageView.addOnAttachStateChangeListener(loader);
-        imageView.setImageDrawable(loader.mDraweeHolder.getTopLevelDrawable());
-
+        DraweeLoader loader = new DraweeLoaderBuilder(context)
+                .setrResize(resize, resize)
+                .setPlaceholder(placeholder)
+                .setUri(uri)
+                .build();
+        loader.load(imageView);
     }
 
     @Override
@@ -71,67 +59,37 @@ public class FrescoEngine implements ImageEngine {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
     public class DraweeLoader implements View.OnAttachStateChangeListener {
 
-        private ForwardingDrawable mActualDrawable;
+        private DraweeHolder<GenericDraweeHierarchy> mDraweeHolder;
+        private DraweeLoaderBuilder builder;
 
-        private int size = 0;
-
-        public DraweeHolder<GenericDraweeHierarchy> mDraweeHolder;
-
-        public DraweeLoader(Context context) {
-            init(context);
+        public DraweeLoader(DraweeLoaderBuilder builder) {
+            this.builder = builder;
+            initFresco();
+            initHolder();
+            initController();
         }
 
-        public void setSize(int size){
-            this.size = size;
+        private void initFresco() {
+            if (!Fresco.hasBeenInitialized()) {
+                Fresco.initialize(builder.getContext());
+            }
         }
 
-        private void init(Context context) {
-            GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(context.getResources())
-//                    .setPlaceholderImage()
+        private void initHolder() {
+            GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(builder.getContext().getResources())
+                    .setPlaceholderImage(builder.getPlaceholder())
                     .build();
-            mDraweeHolder = DraweeHolder.create(hierarchy, context);
-
+            mDraweeHolder = DraweeHolder.create(hierarchy, builder.getContext());
         }
 
-        public void setImageUri(Uri uri) {
-
-            ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
-                    .setResizeOptions(new ResizeOptions(size,size))
+        private void initController() {
+            ImageRequest request = ImageRequestBuilder.newBuilderWithSource(builder.getUri())
+                    .setResizeOptions(builder.getResize())
                     .build();
+
             DraweeController draweeController = Fresco.newDraweeControllerBuilder()
                     .setOldController(mDraweeHolder.getController())
                     .setImageRequest(request)
-                    .setControllerListener(new ControllerListener<ImageInfo>() {
-                        @Override
-                        public void onSubmit(String id, Object callerContext) {
-
-                        }
-
-                        @Override
-                        public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
-
-                        }
-
-                        @Override
-                        public void onIntermediateImageSet(String id, ImageInfo imageInfo) {
-
-                        }
-
-                        @Override
-                        public void onIntermediateImageFailed(String id, Throwable throwable) {
-
-                        }
-
-                        @Override
-                        public void onFailure(String id, Throwable throwable) {
-
-                        }
-
-                        @Override
-                        public void onRelease(String id) {
-
-                        }
-                    })
                     .build();
             mDraweeHolder.setController(draweeController);
         }
@@ -144,6 +102,59 @@ public class FrescoEngine implements ImageEngine {
         @Override
         public void onViewDetachedFromWindow(View v) {
             mDraweeHolder.onDetach();
+        }
+
+        public void load(ImageView targetView) {
+            targetView.addOnAttachStateChangeListener(this);
+            targetView.setImageDrawable(mDraweeHolder.getTopLevelDrawable());
+        }
+
+    }
+
+    public class DraweeLoaderBuilder {
+        public Context mContext;
+        private ResizeOptions mResizeOptions;
+        private Drawable mPlaceHolder;
+        private Uri mUri;
+
+        public DraweeLoaderBuilder(Context context) {
+            this.mContext = context;
+        }
+
+        public Context getContext() {
+            return mContext;
+        }
+
+        public DraweeLoaderBuilder setUri(Uri uri) {
+            this.mUri = uri;
+            return this;
+        }
+
+        public Uri getUri() {
+            return mUri;
+        }
+
+        public DraweeLoaderBuilder setrResize(int width, int height) {
+            mResizeOptions = new ResizeOptions(width, height);
+            return this;
+        }
+
+        public ResizeOptions getResize() {
+            return mResizeOptions;
+        }
+
+        public DraweeLoaderBuilder setPlaceholder(Drawable drawable) {
+            this.mPlaceHolder = drawable;
+            return this;
+        }
+
+        public Drawable getPlaceholder() {
+            return mPlaceHolder;
+        }
+
+
+        public DraweeLoader build() {
+            return new DraweeLoader(this);
         }
     }
 }
