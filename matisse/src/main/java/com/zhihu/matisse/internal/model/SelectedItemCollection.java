4810de85db7d4a90e9b24e0bce261c5b43036d49
REBOOTERS <foreverlove.zyl@163.com>
2018-07-11 18:04:03 +0800

    fix R NoClassDefFound bug

--- matisse/src/main/java/com/zhihu/matisse/internal/model/SelectedItemCollection.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/model/SelectedItemCollection.java
@@ -182,6 +182,11 @@ public class SelectedItemCollection {
                         R.string.error_over_count,
                         maxSelectable
                 );
+            } catch (NoClassDefFoundError e) {
+                cause = mContext.getString(
+                        R.string.error_over_count,
+                        maxSelectable
+                );
             }
 
             return new IncapableCause(cause);
