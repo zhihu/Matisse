8925ee605d1df98e40bc8686dc73027019ae7501
REBOOTERS <foreverlove.zyl@163.com>
2019-10-18 20:33:12 +0800

    delete useless code

--- sample/src/main/java/com/zhihu/matisse/sample/SampleActivity.java
+++ sample/src/main/java/com/zhihu/matisse/sample/SampleActivity.java
@@ -95,16 +95,13 @@ public class SampleActivity extends AppCompatActivity implements View.OnClickLis
                         .thumbnailScale(0.85f)
                         .imageEngine(new GlideEngine())
                         .setOnSelectedListener((uriList, pathList) -> {
-                            // DO SOMETHING IMMEDIATELY HERE
                             Log.e("onSelected", "onSelected: pathList=" + pathList);
-
                         })
                         .showSingleMediaType(true)
                         .originalEnable(true)
                         .maxOriginalSize(10)
                         .autoHideToolbarOnSingleTap(true)
                         .setOnCheckedListener(isChecked -> {
-                            // DO SOMETHING IMMEDIATELY HERE
                             Log.e("isChecked", "onCheck: isChecked=" + isChecked);
                         })
                         .forResult(REQUEST_CODE_CHOOSE);
@@ -125,9 +122,6 @@ public class SampleActivity extends AppCompatActivity implements View.OnClickLis
                 Matisse.from(SampleActivity.this)
                         .choose(MimeType.of(MimeType.GIF), false)
                         .countable(true)
-                        .capture(true)
-                        .captureStrategy(
-                                new CaptureStrategy(true, "com.zhihu.matisse.sample.fileprovider", "test"))
                         .maxSelectable(9)
                         .addFilter(new GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
                         .gridExpectedSize(
@@ -135,19 +129,10 @@ public class SampleActivity extends AppCompatActivity implements View.OnClickLis
                         .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
                         .thumbnailScale(0.85f)
                         .imageEngine(new GlideEngine())
-                        .setOnSelectedListener((uriList, pathList) -> {
-                            // DO SOMETHING IMMEDIATELY HERE
-                            Log.e("onSelected", "onSelected: pathList=" + pathList);
-
-                        })
                         .showSingleMediaType(true)
                         .originalEnable(true)
                         .maxOriginalSize(10)
                         .autoHideToolbarOnSingleTap(true)
-                        .setOnCheckedListener(isChecked -> {
-                            // DO SOMETHING IMMEDIATELY HERE
-                            Log.e("isChecked", "onCheck: isChecked=" + isChecked);
-                        })
                         .forResult(REQUEST_CODE_CHOOSE);
                 break;
             default:
